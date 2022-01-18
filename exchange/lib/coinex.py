48734bd6390da6ea1b658ca81d6119679cf33e69
armansaheb@gmail.com <armansaheb94@gmail.com>
2022-01-18 20:26:23 +0330

    update

--- exchange/lib/coinex.py
+++ exchange/lib/coinex.py
@@ -145,8 +145,8 @@ class CoinEx:
     def order_pending_cancel(self, market, id):
         return self._v1('order/pending', method='delete', auth=True, market=market, id=id)
     
-    def order_stop_pending_cancel(self, market, account_id, id):
-        return self._v1('order/stop/pending', method='delete', auth=True, market=market, account_id=account_id, id=id)
+    def order_stop_pending_cancel(self, market, id):
+        return self._v1('order/stop/pending', method='delete', auth=True, market=market, id=id)
 
     def order_pending_cancel_all(self, account_id, market):
         return self._v1('order/pending', method='delete', auth=True, account_id=account_id, market=market)
