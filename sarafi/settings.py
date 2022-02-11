d1acd43bf7ed052b5f62fe55d3efa763bf285130
armansaheb@gmail.com <armansaheb94@gmail.com>
2022-02-11 17:57:53 +0330

    update

--- sarafi/settings.py
+++ sarafi/settings.py
@@ -24,7 +24,7 @@ ROOT = 'https://amizax.com'
 SECRET_KEY = 'django-insecure-ra$$l%4d2y4i!5#=(n58_u988+gdn8$st2^rtx-l0buiwhi7kc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['amizax.com', 'www.amizax.com' , 'arsdev.ir', 'www.arsdev.ir']
 
@@ -74,7 +74,6 @@ CORS_ALLOWED_ORIGINS = [
     "https://www.arsdesign.ir",
     "http://arsdesign.ir",
     "http://www.arsdesign.ir",
-    "http://localhost:8080"
 ]
 
 MIDDLEWARE = [
