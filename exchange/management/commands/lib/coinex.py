24c9ca772966f7ff6748ad6d65833aac71a2a9cf
armansaheb@gmail.com <armansaheb94@gmail.com>
2022-02-14 15:50:26 +0330

    update

--- exchange/management/commands/lib/coinex.py
+++ exchange/management/commands/lib/coinex.py
@@ -63,7 +63,7 @@ class CoinEx:
         return self._v1('sub_account/auth/api' , method='get', auth=True,sub_user_name = id)
 
     def renew(self ,id ):
-        return self._v1(f'sub_account/auth/api/{id}' , method='put', auth=True , allow_trade = True , allowed_ips = [] )
+        return self._v1(f'sub_account/auth/api/{id}' , method='put', auth=True , sub_user_name = id,  allow_trade = True , allowed_ips = [] )
 
     def margin_loan_history(self, **params):
         return self._v1('margin/loan/history', auth=True, **params)
