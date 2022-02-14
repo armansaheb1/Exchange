3eb5c30e0d7bfb21b8e9627d023c0c3c3d442e15
armansaheb@gmail.com <armansaheb94@gmail.com>
2022-02-14 15:02:16 +0330

    update

--- exchange/management/commands/renew.py
+++ exchange/management/commands/renew.py
@@ -9,7 +9,7 @@ from .lib.coinex import CoinEx
 class Command(BaseCommand):
     def handle(self, *args, **options):
         coinex = CoinEx('56255CA42286443EB7D3F6DB44633C25', '30C28552C5B3337B5FC0CA16F2C50C4988D47EA67D03C5B7' )
-        result = coinex.apis()
+        result = coinex.apis()['data'][0]['user_auth_id']
         print(result)
         return
         robot = CoinexPerpetualApi('56255CA42286443EB7D3F6DB44633C25', '30C28552C5B3337B5FC0CA16F2C50C4988D47EA67D03C5B7')
